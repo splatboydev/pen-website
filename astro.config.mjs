39b688dbff6707e901862aea9d7fb3f89199ad6a
MelnCat <melncatuwu@gmail.com>
2025-12-28 18:21:33 -0800

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import mailObfuscation from "astro-mail-obfuscation";
+
+export default defineConfig({
+	experimental: {
+		fonts: [
+			{
+				name: "Glacial Indifference",
+				cssVariable: "--font-glacial",
+				provider: "local",
+				variants: [
+					{
+						src: ["./src/assets/fonts/GlacialIndifference-Regular.otf"],
+						weight: 400,
+					},
+					{
+						src: ["./src/assets/fonts/GlacialIndifference-Bold.otf"],
+						weight: 700,
+					},
+				],
+			},
+			{
+				name: "Montserrat",
+				cssVariable: "--font-montserrat",
+				provider: fontProviders.google(),
+				weights: [700],
+			},
+		],
+	},
+	integrations: [mailObfuscation()],
+});
